8c9d228f27a419156decb0ce0158c2c7b731e0c0
Saptarinnn <saptarino708@gmail.com>
2024-05-23 02:07:53 +0800

    build vite

--- resources/views/app/incoming_document/index.blade.php
+++ resources/views/app/incoming_document/index.blade.php
@@ -2,11 +2,11 @@
     <x-card title="{{ $title }}" href="{{ route('incoming-document.create') }}">
 
         @if (session('message'))
-            <div class="alert alert-success alert-dismissible" role="alert">
-                {{ session('message') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                </button>
-            </div>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            {{ session('message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+            </button>
+        </div>
         @endif
 
         <x-table>
@@ -21,18 +21,19 @@
             </x-slot>
 
             @foreach ($datas as $data)
-                <tr>
-                    <td class="text-center">{{ $data->document_code }}</td>
-                    <td>{{ convertYmdToMdy($data->date_in) }}</td>
-                    <td>{{ ucwords($data->category->category_name) }}</td>
-                    <td>{{ ucwords($data->storage->storage_name) }}</td>
-                    <td><a href="{{ Storage::url($data->file) }}">Unduh Dokumen</a></td>
-                    <td>{{ ucwords($data->user->fullname) }}</td>
-                    <td>
-                        {{-- <x-table.btn-show href="{{ route('incoming-document.show', $data->id) }}" /> --}}
-                        <x-table.btn-delete action="{{ route('incoming-document.destroy', $data->id) }}" />
-                    </td>
-                </tr>
+            <tr>
+                <td class="text-center">{{ $data->document_code }}</td>
+                <td>{{ convertYmdToMdy($data->date_in) }}</td>
+                <td>{{ ucwords($data->category->category_name) }}</td>
+                <td>{{ ucwords($data->storage->storage_name) }}</td>
+                <td><a href="{{ Storage::url($data->file) }}">Unduh Dokumen</a></td>
+                <td>{{ ucwords($data->user->fullname) }}</td>
+                <td>
+                    {{--
+                    <x-table.btn-show href="{{ route('incoming-document.show', $data->id) }}" /> --}}
+                    <x-table.btn-delete action="{{ route('incoming-document.destroy', $data->id) }}" />
+                </td>
+            </tr>
             @endforeach
 
         </x-table>
--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -3,8 +3,11 @@
 <div class="card mb-4">
     <div class="card-header d-flex justify-content-between">
         <h4 class="card-title fw-semibold">{{ $title }}</h4>
+
+        @if(auth()->user()->can('incoming-document.create') && auth()->user()->can('outgoing-document.create'))
         @if ($create)
-            <a href="{{ $href }}" class="btn btn-sm btn-primary">Tambah Data</a>
+        <a href="{{ $href }}" class="btn btn-sm btn-primary">Tambah Data</a>
+        @endif
         @endif
     </div>
     <div class="card-body">
