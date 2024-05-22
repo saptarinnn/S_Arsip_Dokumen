2d026aaf66b9a49e790294a505e6b64de436f0b9
Saptarinnn <saptarino708@gmail.com>
2024-05-23 02:44:17 +0800

    build vite

--- resources/views/components/card/index.blade.php
+++ resources/views/components/card/index.blade.php
@@ -4,7 +4,10 @@
     <div class="card-header d-flex justify-content-between">
         <h4 class="card-title fw-semibold">{{ $title }}</h4>
 
-        @if(auth()->user()->can('incoming-document.create') && auth()->user()->can('outgoing-document.create'))
+        @if(auth()->user()->can('incoming-document.create') || auth()->user()->can('outgoing-document.create') ||
+        auth()->user()->can('permission.create') || auth()->user()->can('role.create') ||
+        auth()->user()->can('user.create') || auth()->user()->can('category.create') ||
+        auth()->user()->can('storage.create'))
         @if ($create)
         <a href="{{ $href }}" class="btn btn-sm btn-primary">Tambah Data</a>
         @endif
